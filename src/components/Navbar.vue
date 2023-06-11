27c7c7a4f346b7724452b4bf130ce2688412346a
Kersti Miller <kersti.miller@bigbank.ee>
2023-06-11 20:57:50 +0300

    Conditional views based on user

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,17 +4,18 @@
       <h1>My Book List</h1>
 
       <!-- for logged in users -->
-      <div>
+      <div v-if="user">
         <router-link to="/">Home</router-link>
         <button @click=" handleClick">Logout</button>
       </div>
       
       <!-- for logged out users -->
-      <div>
+      <div v-else>
         <router-link to="/login">Login</router-link>
         <router-link to="/signup">Signup</router-link>
       </div>
     </nav>
+    <div v-if="user"> Logged in as {{ user.email }}</div>
   </div>
 </template>
 
